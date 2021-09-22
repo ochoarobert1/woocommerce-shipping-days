07d48cc344d235d4885a671ed95b919a67ef6c8b
Robert Ochoa <ochoa.robert1@gmail.com>
2021-09-22 10:48:03 -0400

    Code Init

---
+++ woocommerce-shipping-days.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Dias de Despacho
+ * Plugin URI: https://robertochoaweb.com/
+ * Description: Plugin para seleccion de dias de despacho.
+ * Version: 1.0.0
+ * Author: Robert Ochoa
+ * Author URI: https://robertochoaweb.com
+ * Text Domain: woo_shipping_days
+ * Domain Path: /languages/
+ * Requires at least: 5.5
+ * Requires PHP: 7.0
+ *
+ * @package WooShipDays
+ */
+
+
+class WooShipDays
+{
+}
